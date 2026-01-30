af832bda0800238d6f2c8451f636717f931f2196
Charged <90069597+ItsCharged@users.noreply.github.com>
2026-01-30 11:06:28 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// Wenn das Repo 'itscharged.github.io' heißt:
 export default defineConfig({
   plugins: [react()],
-  base: '/musikwuensche/',
+  base: '/', 
 })
